04a9abc36fb6f72931d500bdff091370c18ed1af
kakcho <148219495+kakcho@users.noreply.github.com>
2024-06-17 16:19:03 +0300

    init

--- src/context/ApiDataProvider.jsx
+++ src/context/ApiDataProvider.jsx
@@ -6,7 +6,7 @@ import { ApiDataContext } from './ApiDataContext';
 
 const ApiDataProvider = ({children}) => {
         const [data, setData] = useState(null);
-        const testInit = 'TGMA user=%7B%22id%22%3A869219969%2C%22first_name%22%3A%22%F0%9F%A7%9D%E2%80%8D%E2%99%82%EF%B8%8F%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22daaya1%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=-8318441471056915332&chat_type=sender&auth_date=1718628585&hash=a99c4ec339efe1d1e5d8c7a0a538a2a5360b60b90bfc81fb67eff4d28bfad62d'
+        const testInit = 'user=%7B%22id%22%3A869219969%2C%22first_name%22%3A%22%F0%9F%A7%9D%E2%80%8D%E2%99%82%EF%B8%8F%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22daaya1%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=-8318441471056915332&chat_type=sender&auth_date=1718628585&hash=a99c4ec339efe1d1e5d8c7a0a538a2a5360b60b90bfc81fb67eff4d28bfad62d'
         let config = {
         headers: {
             Authorization:  testInit
