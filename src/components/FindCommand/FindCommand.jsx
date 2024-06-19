61c3678cf00deae33ac521796bdd7862cab8f6ec
kakcho <148219495+kakcho@users.noreply.github.com>
2024-06-19 16:22:31 +0300

    public

--- src/components/FindCommand/FindCommand.jsx
+++ src/components/FindCommand/FindCommand.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import './FindCommand.css'
+import FindContent from '../../assets/FindContent.png'
+import people from "../../assets/peopl.svg";
+import age from '../../assets/Age.svg'
 
-const FindCommand = () => {
+const FindCommand = ({profile}) => {
   return (
-    <div>FindCommand</div>
-  )
-}
+    <div className="FindCommandContainer">
+      <h1 className="FindCommandH1">Поиск</h1>
+      <p className="FindCommandName">Elan</p>
+      <div className="FindCommandContent">
+      <div className="FindCommandBlock"><img src={people}  className="FindCommandPeople"/>- 2</div>
+      <div className="FindCommandBlockAge"><img src={age} className="FindCommandPeople"/>- 18</div>
+      <div className="FindCommandBlockRang"></div>
+      <img src={FindContent} className="FindContentImg" />
+      </div>
+    </div>
+  );
+};
 
-export default FindCommand
+export default FindCommand;
